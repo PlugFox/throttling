263cdb3ac9f49749b6ec6abd32975234377a72d8
Plague Fox <plugfox@gmail.com>
2019-06-11 01:03:30 +0300

    init

---
+++ lib/throttling.dart
@@ -0,0 +1,116 @@
+library throttling;
+
+/*
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * MIT License
+ *
+ * Copyright (c) 2019 1C:Awesome
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+import 'dart:async';
+
+///  Throttling
+///  Have method [throttle]
+class Throttling {
+  Duration _duration;
+  Duration get duration => this._duration;
+  set duration(Duration value) {
+    assert(duration is Duration && !duration.isNegative);
+    this._duration = value;
+  }
+  bool _isReady = true;
+  bool get isReady => isReady;
+  Future<void> get _waiter => Future.delayed(this._duration);
+  // ignore: close_sinks
+  final StreamController<bool> _stateSC = new StreamController<bool>.broadcast();
+
+  Throttling({Duration duration = const Duration(seconds: 1)})
+  : assert(duration is Duration && !duration.isNegative)
+  , this._duration = duration ?? Duration(seconds: 1)
+  {
+    this._stateSC.sink.add(true);
+  }
+
+  dynamic throttle(Function func) {
+    if (!this._isReady) return null;
+    this._stateSC.sink.add(false);
+    this._isReady = false;
+    _waiter..then((_) {
+      this._isReady = true;
+      this._stateSC.sink.add(true);
+    });
+    return Function.apply(func, []);
+  }
+
+  StreamSubscription<bool> listen(Function(bool) onData) => this._stateSC.stream.listen(onData);
+
+  dispose() {
+    this._stateSC.close();
+  }
+}
+
+///  Debouncing
+///  Have method [debounce]
+class Debouncing {
+  Duration _duration;
+  Duration get duration => this._duration;
+  set duration(Duration value) {
+    assert(duration is Duration && !duration.isNegative);
+    this._duration = value;
+  }
+  Timer _waiter;
+  bool _isReady = true;
+  bool get isReady => isReady;
+  // ignore: close_sinks
+  StreamController<dynamic> _resultSC = new StreamController<dynamic>.broadcast();
+  // ignore: close_sinks
+  final StreamController<bool> _stateSC = new StreamController<bool>.broadcast();
+
+  Debouncing({Duration duration = const Duration(seconds: 1)})
+  : assert(duration is Duration && !duration.isNegative)
+  , this._duration = duration ?? Duration(seconds: 1)
+  {
+    this._stateSC.sink.add(true);
+  }
+
+  Future<dynamic> debounce(Function func) async {
+    if (this._waiter?.isActive ?? false) {
+      this._waiter?.cancel();
+      this._resultSC.sink.add(null);
+    }
+    this._isReady = false;
+    this._stateSC.sink.add(false);
+    this._waiter = Timer(this._duration, () {
+      this._isReady = true;
+      this._stateSC.sink.add(true);
+      this._resultSC.sink.add(Function.apply(func, []));
+    });
+    return this._resultSC.stream.first;
+  }
+
+  StreamSubscription<bool> listen(Function(bool) onData) => this._stateSC.stream.listen(onData);
+
+  dispose() {
+    this._resultSC.close();
+    this._stateSC.close();
+  }
+}
---
+++ test/throttling_test.dart
@@ -0,0 +1,111 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:throttling/throttling.dart';
+import 'dart:async';
+
+void main() {
+  test('Throttling', () async {
+    print('# Throttling test');
+    final Throttling thr = new Throttling(duration: Duration(seconds: 1));
+    expect(thr is Throttling, true);
+    expect(thr.duration is Duration, true);
+    expect(thr.duration == Duration(seconds: 1), true);
+
+    thr.duration = Duration(seconds: 3);
+    expect(thr.duration == Duration(seconds: 3), true);
+
+    int numberOfAllStates = 0;
+    int numberOfReadyStates = 0;
+    int numberOfBusyStates = 0;
+    StreamSubscription<bool> subscription = thr.listen((bool state) {
+      expect(state is bool, true);
+      print(' *throttling#${thr.hashCode.toRadixString(36)} is ${state ? 'ready' : 'busy'}');
+      numberOfAllStates++;
+      if (state) {
+        numberOfReadyStates++;
+      } else {
+        numberOfBusyStates++;
+      }
+    });
+
+    dynamic result;
+    Future<void> pause([int sec = 1]) => Future.delayed(Duration(seconds: sec)).whenComplete(() => null);
+
+    result = thr.throttle(() {print('. 1'); return 1;});
+    expect(result, 1);
+    await pause();
+
+    result = thr.throttle(() {print('. 2'); return 2;});
+    expect(result, null);
+    await pause();
+
+    result = thr.throttle(() {print('. 3'); return 3;});
+    expect(result, null);
+    await pause();
+
+    result = thr.throttle(() {print('. 4'); return 4;});
+    expect(result, 4);
+    await pause();
+
+    result = thr.throttle(() {print('. 5'); return 5;});
+    expect(result, null);
+    await pause();
+
+    subscription.cancel();
+    expect(numberOfAllStates, 3);
+    expect(numberOfReadyStates, 1);
+    expect(numberOfBusyStates, 2);
+
+  }, timeout: Timeout(Duration(seconds: 7)));
+
+  test('Debouncing', () async {
+    print('# Debouncing test');
+    final Debouncing deb = new Debouncing(duration: Duration(seconds: 1));
+    expect(deb is Debouncing, true);
+    expect(deb.duration is Duration, true);
+    expect(deb.duration, const Duration(seconds: 1));
+
+    deb.duration = Duration(seconds: 3);
+    expect(deb.duration, const Duration(seconds: 3));
+
+    int numberOfAllStates = 0;
+    int numberOfReadyStates = 0;
+    int numberOfBusyStates = 0;
+    StreamSubscription<bool> subscription = deb.listen((bool state) {
+      expect(state is bool, true);
+      print(' *debouncing#${deb.hashCode.toRadixString(36)} is ${state ? 'ready' : 'busy'}');
+      numberOfAllStates++;
+      if (state) {
+        numberOfReadyStates++;
+      } else {
+        numberOfBusyStates++;
+      }
+    });
+
+    Future<dynamic> result;
+    Future<void> pause([int sec = 1]) => Future.delayed(Duration(seconds: sec)).whenComplete(() => null);
+
+    result = deb.debounce(() {print('. 1'); return 1;});
+    await pause();
+    result.then((dynamic value) {
+      expect(value, null);
+    });
+
+    result = deb.debounce(() {print('. 2'); return 2;});
+    await pause();
+    result.then((dynamic value) {
+      expect(value, null);
+    });
+
+    result = deb.debounce(() {print('. 3'); return 3;});
+    await pause(4);
+    result.then((dynamic value) {
+      expect(value, 3);
+    });
+
+    subscription.cancel();
+    expect(numberOfAllStates, 4);
+    expect(numberOfReadyStates, 1);
+    expect(numberOfBusyStates, 3);
+
+  }, timeout: Timeout(Duration(seconds: 7)));
+}
