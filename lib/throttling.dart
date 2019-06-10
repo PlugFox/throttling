68b5d3efd0e29767c1da53252f3badf1fd333c7d
Plague Fox <7805176+PlugFox@users.noreply.github.com>
2019-06-11 01:19:52 +0300

    Update throttling.dart

--- lib/throttling.dart
+++ lib/throttling.dart
@@ -4,7 +4,7 @@ library throttling;
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * MIT License
  *
- * Copyright (c) 2019 1C:Awesome
+ * Copyright (c) 2019 Plugfox
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
