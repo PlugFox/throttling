0ef25243a756197e15598ba906ce571397147fff
Plague Fox <plugfox@gmail.com>
2020-11-05 19:10:11 +0300

    bump version, example, remove dependency

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import './src/example.dart';
+import 'package:throttling_example/src/example.dart';
 
 void main() async {
   print('\n# BEGIN');
--- test/throttling_test.dart
+++ test/throttling_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:throttling/throttling.dart';
 import 'dart:async';
 
